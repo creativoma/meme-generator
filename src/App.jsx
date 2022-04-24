f8d706d0f5a2f52d20e6d7f93681a7dcc174fe8b
creativoma <ma.marianoalvarez@gmail.com>
2022-04-24 18:04:16 +0200

    Created project with Vite.js and created first componente 'Header'

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import '../src/index.css'
+import Header from "./components/Header";
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}

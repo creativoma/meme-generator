d25598da284a29823d0316e5bc2e853e40800f1e
creativoma <ma.marianoalvarez@gmail.com>
2022-04-25 13:27:50 +0200

    Continuing with the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import '../src/index.css'
 import Header from "./components/Header";
+import Meme from "./components/Meme.jsx";
 
 export default function App() {
     return (
         <div>
             <Header />
+            <Meme />
         </div>
     )
 }
